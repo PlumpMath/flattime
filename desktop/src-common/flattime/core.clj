98546ea4c98c5e07869dd22343ad927a3b0d0960
Eric Fode <eric.fode@autodesk.com>
2015-01-24 22:51:37 -0800

    Got things to display as well as a overlay with the frame rate and number of objects in the world. Working on getting object to be only created on a grid.

--- desktop/src-common/flattime/core.clj
+++ desktop/src-common/flattime/core.clj
@@ -4,19 +4,20 @@
             [play-clj.math :refer :all]
             [play-clj.ui :refer :all]))
 
+(declare flattime main-screen overlay-screen)
+
 (def ^:const world-width 40)
 (def ^:const world-height 40)
 (def ^:const ppb 32)
-
+(def ^:const grid-factor 10)
 (defn create-flatlander [x y]
   (assoc (shape :filled
-                :set-color (color :white)
+                :set-color (color :green)
                 :rect 0 0 ppb ppb)
-        :x (* x ppb)
-        :y (* y ppb)
-        (comment "unitless width and height")
-        :x-feet x
-        :y-feet y
+        :x x
+        :y y
+        :x-feet ppb
+        :y-feet ppb
         :width ppb
         :alive? true
         ))
@@ -33,61 +34,110 @@
   {:x 20 :y 20 :x-feet 1 :y-feet 1 :alive? true :id :b }
   )
 
-
+(def landers [flatlander-a flatlander-b flatlander-c])
 
 (defn create-flatlanders [width height]
-  (for [x (range 0 width 2)
-        y (range 0 height 2)]
-    (create-flatlander x y)))
+  (vec (for [x (range 0 width 2)
+             y (range 0 height 2)]
+         (create-flatlander (* grid-factor x) (* grid-factor y)))))
 
 
 (defn entity-rect
   "take an entity and builds a rectangle around it
    at it's current location"
-  [{:keys [x y x-feet y-feet]} min-distance]
+  [{:keys [x y x-feet y-feet]} range]
   (rectangle
    x
    y
-   (+ min-distance x-feet)
-   (+ min-distance y-feet)))
+   (+ range x-feet)
+   (+ range y-feet)))
+
 
 (fact "entity-rect should return a rectange that is useable"
       (entity-rect flatlander-a 0) => (rectangle 1 2 1 1))
 
 (defn near-entitie?
-  [e1 e2]
+  [e1 e2 range]
   (and (not= (:id e1) (:id e2))
-       (rectangle! (entity-rect e1 1) :overlaps (entity-rect e2 1)) ))
+       (rectangle! (entity-rect e1 range) :overlaps (entity-rect e2 range)) ))
 
 (fact "near-entite? should return true is anything is within
       the min-distance"
-      (near-entitie? flatlander-a flatlander-b) => true
-      (near-entitie? flatlander-a flatlander-c) => false)
+      (near-entitie? flatlander-a flatlander-b 2) => true
+      (near-entitie? flatlander-a flatlander-c 2) => false)
 
 (defn nearest-entities
-  [entities entity min-distance]
-  (filter #(near-entitie? % entity) entities))
+  [entities entity range]
+  (filter #(near-entitie? % entity range) entities))
 
-(defn update-flatlander [entity screen]
-  (if (near-entitie? entity)
-    (if  (mod (rand-int 10) 3)
-      (assoc entity :alive? false))))
+(fact "nearest-entites returns all of the entiies within range of another givin enties"
+      (nearest-entities landers flatlander-a 2) => [flatlander-b]
+      (nearest-entities landers flatlander-a 20) => [flatlander-b flatlander-c])
 
 
+(defn update-flatlander [entity entities]
+  (let [near  (nearest-entities entities entity 2)]
+
+    (if (< (count near) 5)
+      (if (mod (rand-int 10) 3)
+        (assoc entity :alive? false)))))
+
+(defscreen overlay-screen
+  :on-show
+  (fn [screen entities]
+    (update! screen :camera (orthographic) :renderer (stage))
+    [(assoc (label "0" (color :white))
+             :id :fps
+             :y 0
+             :x 5)
+     (assoc (label "0" (color :white))
+            :id :entity-count
+            :y 15
+            :x 5)])
+
+  :on-render
+  (fn [screen entities]
+    (->> (for [entity entities]
+           (case (:id entity)
+             :fps (doto entity (label! :set-text (str (game :fps))))
+             entity))
+         (render! screen)))
+
+  :on-resize
+  (fn [screen entities]
+    (height! screen 600))
+
+
+  :on-update-entity-count
+  (fn [screen entities]
+    (let [count (:entity-count screen)]
+      (->> (for [e entities]
+            (case (:id e)
+            :entity-count (doto e (label! :set-text (str count)))
+            e))
+           (render! screen)))))
 
 (defscreen main-screen
   :on-show
   (fn [screen entities]
-    (create-flatlanders 10 10)
-    (update! screen :renderer (stage) :camera (orthographic)))
+    (update! screen :renderer (stage) :camera (orthographic))
+    (create-flatlanders 10 10))
+
+  :on-key-down
+  (fn [screen entities]
+    (case (:key screen)
+      (key-code :r) (set-screen! flattime main-screen overlay-screen)
+        (conj entities (create-flatlander
+           (rand-int (width screen) )
+           (rand-int (height screen) )))))
 
   :on-render
   (fn [screen entities]
     (clear!)
+    (screen! overlay-screen :on-update-entity-count :entity-count (count entities))
     (->> entities
-      (comment  (map #(update-flatlander %)))
-      (comment  (filter #(:alive? %)))
-      (render! screen)))
+;         (map #(update-flatlander % entities))
+         (render! screen)))
 
   :on-resize
   (fn [screen entities]
@@ -96,7 +146,7 @@
 (defgame flattime
   :on-create
   (fn [this]
-    (set-screen! this main-screen)))
+    (set-screen! this main-screen overlay-screen)))
 
 (defscreen blank-screen
   :on-render
@@ -107,4 +157,4 @@
                        (try (screen-fn)
                             (catch Exception e
                               (.printStackTrace e)
-                              (set-screen! flattime blank-screen)))))
+                              (set-screen! flattime overlay-screen)))))
